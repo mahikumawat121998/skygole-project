20d2fa38d3be1e53ada7eb95f5ec6c09ebddae36
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-14 02:11:49 +0530

    bug fixes

--- client-project/src/component/Dashboard/Dashboard.jsx
+++ client-project/src/component/Dashboard/Dashboard.jsx
@@ -1,17 +1,6 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+
 import UserDetails from "../UserDetails/UserDetails";
 import { useEffect } from "react";
 import axios from "axios";
@@ -19,12 +8,9 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Dashboard() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
   const [userData, setUserData] = React.useState(null);
   const navigate = useNavigate();
   useEffect(() => {
@@ -50,28 +36,8 @@ function Dashboard() {
     fetchUserData();
   }, []);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const handleLogout = () => {
-    localStorage.removeItem("accessToken");
-    // Optional: Clear context or state
-    // logout(); // If using useAuth()
-    // Navigate to login page
-    navigate("/login");
-    toast.success("Logged out successfully");
-  };
+  
+  
 
   return (
     <>
